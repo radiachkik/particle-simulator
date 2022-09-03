91430f4fb899f4b4d81be4b1b3cf2845c622fa5e
radiachkik <radi.achkik@gmail.com>
2022-09-03 17:02:30 +0200

    2d kernel, but not optimized yet

--- simulation.cu
+++ simulation.cu
@@ -11,7 +11,7 @@ namespace simulation {
         __constant__ float *dev_norm_coordinates;
         __constant__ float *dev_forces;
         __constant__ unsigned int dev_num_point_clouds;
-        __constant__ const unsigned int dev_dimensions = 2;
+        __constant__ const unsigned int DEV_DIMENSIONS = 2;
         __constant__ unsigned int dev_points_per_cloud;
         __constant__ float *dev_distance_thresholds;
         __constant__ bool dev_border_enabled;
@@ -26,7 +26,7 @@ namespace simulation {
             unsigned int id = threadIdx.x + blockIdx.x * blockDim.x;
             unsigned int total_threads = blockDim.x * gridDim.x;
 
-            unsigned int total_values = dev_num_point_clouds * dev_points_per_cloud * dev_dimensions;
+            unsigned int total_values = dev_num_point_clouds * dev_points_per_cloud * DEV_DIMENSIONS;
 
             curand_init(134, id, 0, &rand_state[id]);
             curandState localState = rand_state[id];
@@ -40,67 +40,88 @@ namespace simulation {
         {
             unsigned int id = threadIdx.x + blockIdx.x * blockDim.x;
             unsigned int total_threads = blockDim.x * gridDim.x;
-            unsigned int total_values = dev_points_per_cloud * dev_dimensions * dev_num_point_clouds;
+            unsigned int total_values = dev_points_per_cloud * DEV_DIMENSIONS * dev_num_point_clouds;
 
             for(unsigned int i = id; i < total_values; i += total_threads) {
                 dev_norm_coordinates[i] = dev_coordinates[i] / 500;
             }
         }
 
-        template <unsigned int blockSize>
+        template <unsigned int block_size_x, unsigned int block_size_y>
         __global__
         void calculate_forces_kernel()
         {
-            __shared__ float shared_force[blockSize * dev_dimensions];
-            __shared__ float p1[dev_dimensions];
+            __shared__ float shared_force[block_size_y][block_size_x * DEV_DIMENSIONS];
+            __shared__ float partial_pc1[block_size_x * DEV_DIMENSIONS];
+            __shared__ float partial_pc2[block_size_y * DEV_DIMENSIONS];
 
-            // Each block is responsible for calculating the summed forces applied on p1
-            for (auto p1_index = blockIdx.x; p1_index < dev_points_per_cloud * dev_num_point_clouds; p1_index += gridDim.x) {
-                if (threadIdx.x < dev_dimensions) {
-                    p1[threadIdx.x] = dev_coordinates[p1_index * dev_dimensions + threadIdx.x];
+            auto thread_y_id = threadIdx.y + blockIdx.y * blockDim.y;
+            auto thread_x_id = threadIdx.x + blockIdx.x * blockDim.x;
+
+            auto num_y_threads = gridDim.y * blockDim.y;
+            auto num_x_threads = gridDim.x * blockDim.x;
+
+            unsigned int num_points = dev_points_per_cloud * dev_num_point_clouds;
+
+            // Each y block is responsible for calculating the summed forces applied on subset pc1
+            for (auto p1_index = thread_y_id; p1_index < num_points; p1_index += num_y_threads) {
+                if (threadIdx.x == 0) {
+                    for (auto d = 0; d < DEV_DIMENSIONS; d++) {
+                        partial_pc1[threadIdx.y * DEV_DIMENSIONS + d] = dev_coordinates[p1_index * DEV_DIMENSIONS + d];
+                    }
                 }
+                __syncthreads();
+                float force[DEV_DIMENSIONS] = {0.0f};
+                float *p1 = partial_pc1 + threadIdx.y * DEV_DIMENSIONS;
                 auto pc1_index = p1_index / dev_points_per_cloud;
-                float force[dev_dimensions];
-                for (float & d : force) {
-                    d = 0.0f;
-                }
-                // Each thread is responsible for calculating the summed forces applied on p1 for some p2
-                for(auto p2_index = threadIdx.x; p2_index < dev_points_per_cloud * dev_num_point_clouds; p2_index += blockDim.x) {
-                    float *p2 = dev_coordinates + p2_index * dev_dimensions;
+                // Each x block is responsible for calculating the forces between pc1 and a subset pc2
+                for(auto p2_index = thread_x_id; p2_index < dev_points_per_cloud * dev_num_point_clouds; p2_index += num_x_threads) {
+                    if (threadIdx.y == 0) {
+                        for (auto d = 0; d < DEV_DIMENSIONS; d++) {
+                            partial_pc2[threadIdx.x * DEV_DIMENSIONS + d] = dev_coordinates[p2_index * DEV_DIMENSIONS + d];
+                        }
+                    }
+                    __syncthreads();
+
+                    float *p2 = partial_pc2 + threadIdx.x * DEV_DIMENSIONS;
                     auto pc2_index = p2_index / dev_points_per_cloud;
                     float gravity = dev_gravities[pc1_index * dev_num_point_clouds + pc2_index];
                     float distance_threshold = dev_distance_thresholds[pc1_index * dev_num_point_clouds + pc2_index];
-
-                    float distance[dev_dimensions];
-                    for (int d = 0; d < dev_dimensions; d++) {
+                    float distance[DEV_DIMENSIONS];
+                    for (int d = 0; d < DEV_DIMENSIONS; d++) {
                         distance[d] = p2[d] - p1[d];
                     }
-                    float r = normf((int)dev_dimensions, distance);
+                    float r = normf(DEV_DIMENSIONS, distance);
                     if (r < distance_threshold && r >= 1) {
-                        for (int d = 0; d < dev_dimensions; d++) {
+                        for (int d = 0; d < DEV_DIMENSIONS; d++) {
                             force[d] += (distance[d] / (r * 100) ) * gravity;
                         }
                     }
                 }
-                for (int d = 0; d < dev_dimensions; d++) {
-                    shared_force[threadIdx.x * dev_dimensions + d] = force[d];
+
+                // Each y thread represents one p1. Each x thread is a subset of p2's.
+                for (int d = 0; d < DEV_DIMENSIONS; d++) {
+                    shared_force[threadIdx.y][threadIdx.x * DEV_DIMENSIONS + d] = force[d];
                 }
+                __syncthreads();
+
 
                 // Sum up forces in shared memory
-                __syncthreads();
+
                 for (unsigned int s = blockDim.x / 2; s > 0; s >>= 1) {
                     if (threadIdx.x < s) {
-                        for (unsigned int d = 0; d < dev_dimensions; d++) {
-                            shared_force[(threadIdx.x * dev_dimensions) + d] += shared_force[(threadIdx.x + s) * dev_dimensions + d];
+                        for (unsigned int d = 0; d < DEV_DIMENSIONS; d++) {
+                            shared_force[threadIdx.y][(threadIdx.x * DEV_DIMENSIONS) + d] += shared_force[threadIdx.y][(threadIdx.x + s) * DEV_DIMENSIONS + d];
                         }
                     }
                     __syncthreads();
                 }
 
                 // Copy forces to global memory
-                if (threadIdx.x < dev_dimensions) {
-                    dev_forces[p1_index * dev_dimensions + threadIdx.x] = shared_force[threadIdx.x];
+                if (threadIdx.x < DEV_DIMENSIONS) {
+                    dev_forces[p1_index * DEV_DIMENSIONS + threadIdx.x] = shared_force[threadIdx.y][threadIdx.x];
                 }
+                __syncthreads();
             }
         }
 
@@ -109,7 +130,7 @@ namespace simulation {
         {
             unsigned int id = threadIdx.x + blockIdx.x * blockDim.x;
             unsigned int total_threads = blockDim.x * gridDim.x;
-            unsigned int total_values = dev_points_per_cloud * dev_dimensions * dev_num_point_clouds;
+            unsigned int total_values = dev_points_per_cloud * DEV_DIMENSIONS * dev_num_point_clouds;
 
             for(unsigned int i = id; i < total_values; i += total_threads) {
                 float new_velocity = (dev_velocities[i] + dev_forces[i]) * 0.5f;
@@ -188,7 +209,9 @@ namespace simulation {
     }
 
     void simulate_next_frame() {
-        calculate_forces_kernel<512><<<4092, 512>>>();
+        dim3 blockDim(32, 32);
+        dim3 gridDim(128, 128);
+        calculate_forces_kernel<64, 64><<<gridDim, blockDim>>>();
         calculate_new_coordinates_kernel<<<512, 256>>>();
     }
 
